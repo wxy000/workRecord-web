f35d693fa62731c6a20e950407600cb15b124d48
wxy000 <396310583@qq.com>
2022-12-28 21:30:59 +0800

    增加分析页面

---
+++ src/controller/analysis.js
@@ -0,0 +1,15 @@
+layui.define(['table', 'form', 'element', 'laydate', 'upload'], function(exports){
+	var $ = layui.$
+		,admin = layui.admin
+		,view = layui.view
+		,setter = layui.setter
+		,table = layui.table
+		,form = layui.form
+		,laydate = layui.laydate
+		,element = layui.element
+		,upload = layui.upload;
+
+	
+
+	exports('analysis', {})
+});
